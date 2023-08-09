32301e9a04be2e15c13931036b26fd79e114be36
Akash Belekar <akash.belekar@mobifilia.com>
2023-08-09 18:04:26 +0530

    My playground

---
+++ Contents.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+var greeting = "Hello, playground"
+var password = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"][Int.random(in: 0...25)]
+let p1 = password
+let p2 = password
+let p3 = password
+let p4 = password
+let p5 = password
+let p6 = password
+
+let alfa = p1+p2+p3+p4+p5+p6
+
+print(alfa)
+
+
+
